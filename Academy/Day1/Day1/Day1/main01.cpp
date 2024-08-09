9bcf243cb376df42cfc57d321e8d5f176ba56a18
sbscerti <160083857+sbscerti@users.noreply.github.com>
2024-08-09 21:14:43 +0900

    .gitignore 수정, .editorconfig 생성, 내부 컨피그 수정, 학원 방문시 수정용 컨피그 텍스트 생성

---
+++ Academy/Day1/Day1/Day1/main01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	printf("Hello World!\n\n");
+	// \n은 엔터(줄띄움) 효과
+
+	//애국가 1절 출력하기
+	printf("동해물과 백두산이\n마르고 닮도록\n하느님이 보우하사\n우리나라 만세");
+}
+//ctrl + f5로 실행
+
+//프로그래밍은 항상 main함수 실행
+
+//main함수는 프로그래밍 시작점
